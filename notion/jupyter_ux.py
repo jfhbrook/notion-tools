dd7c7fb07f98deaf5bba236aa5b4ea3d09ff0f3d
Josh Holbrook <jholbrook@eaze.com>
2021-12-28 14:31:06 -0500

    ipywidgets configuration editor

--- notion/jupyter_ux.py
+++ notion/jupyter_ux.py
@@ -1,8 +1,37 @@
+import ipywidgets as widgets
+
 from notion.ctx import Context
 
 ctx = Context()
-settings = ctx.settings
-
 
 def get_client():
     return ctx.get_client()
+
+
+def settings():
+    title = widgets.Label(value="Settings")
+    token_field = widgets.Password(
+        description='token:',
+        value=ctx.settings.token
+    )
+    page_field = widgets.Text(
+        description='page:',
+        value=ctx.settings.page
+    )
+    ok_button = widgets.Button(
+        description='Update'
+    )
+
+    modal = widgets.VBox([
+        title,
+        token_field,
+        page_field,
+        ok_button
+    ])
+
+    def update_settings(button):
+        ctx.update_settings(token=token_field.value, page=page_field.value)
+
+    ok_button.on_click(update_settings)
+
+    return modal
